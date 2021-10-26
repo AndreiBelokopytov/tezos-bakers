0f844a68952e120cc75caad372183eddc3c7aaef
Andrei Belokopytov <belokopytov87@gmail.com>
2021-10-26 11:49:16 +0300

    Improve error handling in ApiService

--- TezosBakers/API/Extensions/ApiService + LoadData.swift
+++ TezosBakers/API/Extensions/ApiService + LoadData.swift
@@ -8,24 +8,30 @@
 import Foundation
 
 extension ApiService {
+    static var acceptableHTTPCodes: [Int] {
+        return [200]
+    }
+    
     func loadData(url: URL, completionHandler: OnDataLoaded<Model>?) {
         var errorMessage: String?
         var result: Model?
         let decoder = JSONDecoder()
         
         URLSession.shared.dataTask(with: url) {(data, response, error) in
-            if let data = data {
-                do {
-                    result = try decoder.decode(Model.self, from: data)
-                } catch {
-                    errorMessage = "Can't parse data"
+            guard error == nil,
+                let data = data,
+                let statusCode = (response as? HTTPURLResponse)?.statusCode, Self.acceptableHTTPCodes.contains(statusCode) else {
+                DispatchQueue.main.async {
+                    completionHandler?((nil, error?.localizedDescription ?? "Server returns error"))
                 }
+                return
             }
-            if error != nil {
-                errorMessage = error?.localizedDescription
+
+            do {
+                result = try decoder.decode(Model.self, from: data)
+            } catch {
+                errorMessage = "Can't parse data"
             }
-            
-            // TODO: check response status
             
             DispatchQueue.main.async {
                 completionHandler?((result, errorMessage))
